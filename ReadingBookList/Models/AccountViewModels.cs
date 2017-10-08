faa6d9fbb6e0585ffcc127bb31fa849956c656ae
IamVic <vic.pascual@gmail.com>
2017-10-08 08:01:43 -0400

    Added comments

--- ReadingBookList/Models/AccountViewModels.cs
+++ ReadingBookList/Models/AccountViewModels.cs
@@ -3,26 +3,67 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ReadingBookList.Models
 {
+    /// <summary>
+    /// This class is used for External Login Confirmation
+    /// Used for external login confirmation
+    /// </summary>
     public class ExternalLoginConfirmationViewModel
     {
+        /// <summary>
+        /// The Email Property represents the email of the user
+        /// </summary>
+        /// <value>The Email property gets/sets the value of the string field</value>
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
 
+    /// <summary>
+    /// This class is used for creating the ExternalLoginListViewModel object
+    /// Used for external login
+    /// </summary>
     public class ExternalLoginListViewModel
     {
+        /// <summary>
+        /// The ReturnUrl Property represents the login Url 
+        /// </summary>
+        /// <value>The ReturnUrl property gets/sets the value of the string field</value>
         public string ReturnUrl { get; set; }
     }
 
+    /// <summary>
+    /// This class is used for Sending code when user forgot passwword
+    /// </summary>
     public class SendCodeViewModel
     {
+        /// <summary>
+        /// The SelectedProvider Property
+        /// </summary>
+        /// <value>The SelectedProvider property gets/sets the value of the string field</value>
         public string SelectedProvider { get; set; }
+
+        /// <summary>
+        /// The Providers Property 
+        /// </summary>
+        /// <value>The Providers property gets/sets the value of the ICollection<System.Web.Mvc.SelectListItem> field</value>
         public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+
+        /// <summary>
+        /// The ReturnUrl Property represents the SendCode Url 
+        /// </summary>
+        /// <value>The ReturnUrl property gets/sets the value of the string field</value>
         public string ReturnUrl { get; set; }
+
+        /// <summary>
+        /// The RememberMe Property 
+        /// </summary>
+        /// <value>The RememberMe property gets/sets the value of the boolean field</value>
         public bool RememberMe { get; set; }
     }
 
+    /// <summary>
+    /// This class is used for verifying the code when user use a forget password function
+    /// </summary>
     public class VerifyCodeViewModel
     {
         [Required]
@@ -39,71 +80,132 @@ namespace ReadingBookList.Models
         public bool RememberMe { get; set; }
     }
 
+    /// <summary>
+    /// This class the ViewModel for User's Forgot password
+    /// </summary>
     public class ForgotViewModel
     {
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
-
+    
+    /// <summary>
+    /// This class is the ViewModel for  User Login
+    /// </summary>
     public class LoginViewModel
     {
+        /// <summary>
+        /// The Email Property represents the email of the user
+        /// </summary>
+        /// <value>The Email property gets/sets the value of the string field</value>
         [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
+        /// <summary>
+        /// The Password Property represents the password of the user
+        /// </summary>
+        /// <value>The Password property gets/sets the value of the password field</value>
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        /// <summary>
+        /// The RememberMe Property is used when user wants to remember his/her login credentials
+        /// </summary>
+        /// <value>The RememberMe property gets/sets the value of the boolean field</value>
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
 
+    /// <summary>
+    /// This class is the ViewModel for User Registration
+    /// </summary>
     public class RegisterViewModel
     {
+        /// <summary>
+        /// The Email Property represents the email of the user
+        /// </summary>
+        /// <value>The Email property gets/sets the value of the string field</value>
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
+        /// <summary>
+        /// The Password Property represents the password of the user
+        /// </summary>
+        /// <value>The Password property gets/sets the value of the password field</value>
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        /// <summary>
+        /// The ConfirmPassword Property used for user password confirmation
+        /// </summary>
+        /// <value>The ConfirmPassword property gets/sets the value of the password field</value>
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
+    /// <summary>
+    /// This class is the View Model for User's Resetting Password
+    /// </summary>
     public class ResetPasswordViewModel
     {
+
+        /// <summary>
+        /// The Email Property represents the email of the user
+        /// </summary>
+        /// <value>The Email property gets/sets the value of the string field</value>
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// The Password Property represents the password of the user
+        /// </summary>
+        /// <value>The Password property gets/sets the value of the password field</value>
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        /// <summary>
+        /// The ConfirmPassword Property used for user password confirmation
+        /// </summary>
+        /// <value>The ConfirmPassword property gets/sets the value of the password field</value>
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
+        /// <summary>
+        /// The Code Property
+        /// </summary>
+        /// <value>The Code property gets/sets the value of the string field</value>
         public string Code { get; set; }
     }
 
+    /// <summary>
+    /// This class is the View Model for User's forgot password
+    /// </summary>
     public class ForgotPasswordViewModel
     {
+        /// <summary>
+        /// The Email Property represents the email of the user
+        /// </summary>
+        /// <value>The Email property gets/sets the value of the string field</value>
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
--- ReadingBookList/Models/Book.cs
+++ ReadingBookList/Models/Book.cs
@@ -10,21 +10,67 @@ using System.Web.Mvc;
 
 namespace ReadingBookList.Models
 {
+    /// <summary>
+    /// This class is used for creating a Book object
+    /// </summary>
     public class Book
     {
+        /// <summary>
+        /// The Id Property represents the Book's Id
+        /// </summary>
+        /// <value>The Id property gets/sets the value of the int field</value>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        /// <summary>
+        /// The ISBN Property represents the Book's ISBN
+        /// </summary>
+        /// <value>The ISBN property gets/sets the value of the string field</value> 
         [ValidateISBN(ErrorMessage = "Invalid ISBN Format")]
+
+        /// <summary>
+        /// The ISBN Property represents the Book's ISBN
+        /// </summary>
+        /// <value>The ISBN property gets/sets the value of the string field</value>
         public string ISBN { get; set; }
+
+        /// <summary>
+        /// The Title Property represents the Book's Title/Name
+        /// </summary>
+        /// <value>The Title property gets/sets the value of the string field</value>
         public string Title { get; set; }
+
+        /// <summary>
+        /// The Mark Property represents the Book's Mark (true  if it's read, otherwise it's set to false)
+        /// by the default it is set to false
+        /// </summary>
+        /// <value>The Mark property gets/sets the value of the boolean field</value>
         public bool Mark { get; set; }
+
+        /// <summary>
+        /// The AddedDate Property represents the DateTime when the book was added.
+        /// </summary>
+        /// <value>The Added property gets/sets the value of the DateTime field</value>
         public DateTime? AddedDate { get; set; }
+
+        /// <summary>
+        /// The ModifiedDate Property represents the DateTime when the book was last modified
+        /// </summary>
+        /// <value>The ModifiedDate property gets/sets the value of the DateTime field</value>
         public DateTime? ModifiedDate { get; set; }
 
+        /// <summary>
+        /// The UserId Property represents the Id of the User owner of the book
+        /// </summary>
+        /// <value>The UserId property gets/sets the value of the string field</value>
         [ForeignKey("ApplicationUser")]
         public string UserId { get; set; }
+
+        /// <summary>
+        /// The ApplicationUser Property represents the ApplicationUser of the Book
+        /// </summary>
+        /// <value>The ApplicationUser property gets/sets the value of the ApplicationUser field</value>
         public virtual ApplicationUser ApplicationUser { get; set; }
 
     }
